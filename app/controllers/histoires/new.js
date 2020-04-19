aed72dbac55223c70f01831731ef168f4f3cd94e
bouveyronn <bouveyronn@gmail.com>
2020-04-19 16:36:04 +0200

    POST ok

--- app/controllers/histoires/new.js
+++ app/controllers/histoires/new.js
@@ -3,34 +3,26 @@ import jQuery from 'jquery';
 
 export default Controller.extend({
 
-actions: {
-  ajouter_Histoire () {
+    actions: {
+        ajouter_Histoire() {
 
-    let data1 = {
-      titre: "Test 05/04/2020 de l'insertion d'une histoire via ember",
-      resume: "Résumé de l'insertion, si ce message est visible ne bdd, cela à fonctionner via ember!",
-      est_publiee: true,
-      image: "",
-      fk_id_auteur: 1,
-      etapes: [],
-      genres: []
-    };
-    $.post('http://localhost:8080/histoires', data1).then(function() {
-        alert('ajoute effecuté');
-      });
-
-  }
-}
+            let data1 = {
+                titre: "Test 19/04/2020 de l'insertion d'une histoire via ember",
+                resume: "Résumé de l'insertion, si ce message est visible ne bdd, cela à fonctionner via ember!",
+                est_publiee: false,
+                image: "https://via.placeholder.com/1080",
+                fk_id_auteur: 1,
+                etapes: [],
+                genres: []
+            };
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:8080/histoires",
+                data: JSON.stringify(data1),
+                success: alert("coucou"),
+                dataType: "json",
+                contentType: "application/json",
+            });
+        }
+    }
 });
-
-
-// type: 'post',
-//   data: {
-//   titre: "Test 05/04/2020 de l'insertion d'une histoire via ember",
-//     resume: "Résumé de l'insertion, si ce message est visible ne bdd, cela à fonctionner via ember!",
-//     est_publiee: true,
-//     image: "Ressources/upload/yu-gi-oh.png",
-//     fk_id_auteur: 1,
-//     etapes: [],
-//     genres: []
-// }
