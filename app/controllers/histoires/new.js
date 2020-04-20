85780869404340019bc13178d26a47975b72aa49
hamidi <johanhamididu38@gmail.com>
2020-04-20 15:33:31 +0200

    Creation d'histoire

--- app/controllers/histoires/new.js
+++ app/controllers/histoires/new.js
@@ -2,13 +2,11 @@ import Controller from '@ember/controller';
 import jQuery from 'jquery';
 
 export default Controller.extend({
-
     actions: {
         ajouter_Histoire() {
-
             let data1 = {
-                titre: "Test 19/04/2020 de l'insertion d'une histoire via ember",
-                resume: "Résumé de l'insertion, si ce message est visible ne bdd, cela à fonctionner via ember!",
+                titre: this.get('titre'),
+                resume: this.get('resume'),
                 est_publiee: false,
                 image: "https://via.placeholder.com/1080",
                 fk_id_auteur: 1,
@@ -19,7 +17,7 @@ export default Controller.extend({
                 type: "POST",
                 url: "http://localhost:8080/histoires",
                 data: JSON.stringify(data1),
-                success: alert("coucou"),
+                success: alert("histoire ajoutée"),
                 dataType: "json",
                 contentType: "application/json",
             });
