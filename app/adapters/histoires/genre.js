75350b7a216be5f018e3ebd00f6188f6a1390c0c
mathis69400 <45007491+mathis69400@users.noreply.github.com>
2020-06-17 13:38:04 +0200

    update recherche par genres

--- app/adapters/histoires/genre.js
+++ app/adapters/histoires/genre.js
@@ -6,8 +6,6 @@ const Api = 'http://localhost:8080';
 // eslint-disable-next-line ember/new-module-imports
 export default Ember.Object.extend({
   findHistoiresGenre: function (genre_id) {
-      var jsonGenre = ajax(Api + '/histoire-genre/' + genre_id);
-      console.log(jsonGenre);
       return ajax(Api + '/histoire-genre/' + genre_id);
   }
 });
--- app/routes/histoires/genre.js
+++ app/routes/histoires/genre.js
@@ -2,34 +2,10 @@ import Route from '@ember/routing/route';
 import GenreAdapter from 'story-heroes/adapters/histoires/genre';
 import HistoireAdapter from 'story-heroes/adapters/histoires/detail';
 
-async function getData(idHistoire){
-  var adapter = HistoireAdapter.create();
-  return await adapter.find(idHistoire);
-}
-
 export default Route.extend({
   model: function(params){
     var adapter = GenreAdapter.create();
+    console.log(adapter.findHistoiresGenre(params.genre_id));
     return adapter.findHistoiresGenre(params.genre_id);
-  },
-  afterModel(model) {
-
-    var jsonOuput=null;
-
-    for (var i=0; i<model.length; i++){
-      jsonOuput = function (){
-          getData(model[i].histoire).then((response) => {
-          try {
-            console.log(response);
-          }
-          catch{
-            alert("Wallah ca marche pas");
-          }
-        });
-      };
-
-    }
-
-
   }
 });
